90b81434b528ef15f0e1e46d0d7089d693412c10
ErikNoren05 <91731222+ErikNoren05@users.noreply.github.com>
2022-08-31 14:21:43 +0200

    Added WriteToTile function
    read from text file

--- readandwrite/src/main.cpp
+++ readandwrite/src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 
 namespace Lib
 {
@@ -9,12 +12,21 @@ namespace Lib
 		std::cout << t << "\n";
 	}
 
+	void WriteToFile(std::string filePath)
+	{
+		std::ofstream file(filePath);
+		std::string fileContent;
+		std::getline(std::cin, fileContent); 
+		file << fileContent;
+		file.close();
+	}
 
-
+	std::string filepath1 = "minfil.txt";
 }
 
 
 int main() 
 {
 	Lib::print(123);
+	Lib::WriteToFile(Lib::filepath1);
 }
